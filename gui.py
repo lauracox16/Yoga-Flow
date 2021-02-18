51adc629401e1c40f8c13dd65ba2ac7368bbdd10
Noah <coomsy@protonmail.com>
2021-02-17 22:24:56 -0500

    v0.1 restruct

--- gui.py
+++ gui.py
@@ -2,70 +2,80 @@ from datetime import datetime
 import os
 from PIL import ImageTk, Image
 import PIL.Image
-from poselist import *
-from poselist import poseIllustrations
 import sys
 import time
 from tkinter import *
 from tkinter import ttk as ttk
 import tkinter as tk
 import tkinter
+import json
+from io import BytesIO
+
+import requests
+from PIL import ImageTk, Image
+import PIL.Image
 
 
+counter = 0
+running = False
 
-#___________________________________________________________________________________________________________________________________________________________
-
-# Creating the pop up window
 root = tk.Tk()
 root.title('Yoga Flow Generator')
-
 
 canvas = tk.Canvas(root, height=700, width=700, bg = '#FA8072')
 canvas.pack()
 
-
 frame = tk.Frame(root, bg='white')
 frame.place(relwidth=0.8, relheight=0.8, relx = 0.09, rely=0.07)
 
-#___________________________________________________________________________________________________________________________________________________________
+# Creating the pop up window
 
 # Adding images to GUI
-path = "Pose Illustrations/High Poses/Chair.PNG"
+base = "pose_illustrations/leet_poses/yoga_master.jpg"
 
-img = ImageTk.PhotoImage(PIL.Image.open(path))
+#buffer = BytesIO()
+#base = requests.get('URL').content
+#buffer.write(b'')
+#buffer.seek(0)
+
+img = ImageTk.PhotoImage(PIL.Image.open(base))
 
 panel = tk.Label(frame, bg='white', image = img, width=500, height=500)
 
 panel.pack()
 panel.place(x=30, y=30)
 
-# Pulling the illustrations from poselist.py
-poseIllustrations = iter(poseIllustrations)
+
+_images = []
+for filepath, _, files in list(os.walk('pose_illustrations')):
+    for img in files:
+        _images.append(filepath.replace('\\','/') + '/' + img)
+
+images = iter(_images)
+
 
 def next_img():
+    global images
+    
     try:
-        img = next(poseIllustrations)  # gets next image from iterator
+        img = next(images)  # gets next image from iterator
     except StopIteration:
-        return  # if no more images, nothing happens
+        #return  # if no more images, nothing happens
+        images = iter(_images)
+        img = next(images)
+    except FileNotFoundError:
+        # online
+        return
 
+    root.title('NOW DO THE [' + img.split('/')[-1].split('.')[0] + ']')
     # load and display image
     img = PIL.Image.open(img)
     img = ImageTk.PhotoImage(img)
     panel.img = img
     panel['image'] = img
 
-illustrationButton = tk.Button(frame, relief=FLAT, text='NEXT POSITION',bg="#FFC3B3", fg='#595858', width=30, font='avory 10 bold', command=next_img)
-illustrationButton.pack()
-
-
-# show the first image
-next_img()
-
-#___________________________________________________________________________________________________________________________________________________________
 
 # Creating a min:sec stop watch
-counter = 0
-running = False
 def counter_label(label):
     def count():
         if running:
@@ -87,6 +97,7 @@ def counter_label(label):
     # Triggering the counter
     count()
 
+
 # Start function of the stopwatch
 def Start(label):
     global running
@@ -96,6 +107,7 @@ def Start(label):
     stop['state'] = 'normal'
     reset['state'] = 'normal'
 
+
 # Stop function of the stopwatch
 def Stop():
     global running
@@ -103,6 +115,7 @@ def Stop():
     stop['state'] = 'disabled'
     reset['state'] = 'normal'
     running = False
+
 
 # Reset function of the stopwatch
 def Reset(label):
@@ -119,20 +132,31 @@ def Reset(label):
         label['text'] = '00:00'
 
 
-# Customizing buttons
-label = tk.Label(frame, relief = FLAT, text='00:00', fg='#FFC3B3', bg='white', font='avory 25 bold')
-start = tk.Button(frame, relief = FLAT, text='START', fg='#595858', bg='#FFC3B3', width=5, font='avory 8 bold', command=lambda:Start(label))
-stop = tk.Button(frame, relief = FLAT, text='STOP', fg='#595858', bg='#FFC3B3', width=5, font='avory 8 bold', state='disabled', command=Stop)
-reset = tk.Button(frame, relief = FLAT, text='RESET', fg='#595858', bg='#FFC3B3', width=5, font='avory 8 bold', state='disabled', command=lambda:Reset(label))
-label.pack()
-start.pack()
-stop.pack()
-reset.pack()
-label.place(x=30, y=480)
-start.place(x=5, y=530)
-stop.place(x=52, y=530)
-reset.place(x=99, y=530)
+if __name__ == "__main__":
 
-#___________________________________________________________________________________________________________________________________________________________
 
-root.mainloop()
+    # Pulling the illustrations from poselist.py
+    #pose_pics = list(os.walk('pose_illustrations')[1:])
+
+    illustrationButton = tk.Button(frame, relief=FLAT, text='NEXT POSITION',bg="#FFC3B3", fg='#595858', width=30, font='avory 10 bold', command=next_img)
+    illustrationButton.pack()
+
+    # show the first image
+    next_img()
+
+    
+    # Customizing buttons
+    label = tk.Label(frame, relief = FLAT, text='00:00', fg='#FFC3B3', bg='white', font='avory 25 bold')
+    start = tk.Button(frame, relief = FLAT, text='START', fg='#595858', bg='#FFC3B3', width=5, font='avory 8 bold', command=lambda:Start(label))
+    stop = tk.Button(frame, relief = FLAT, text='STOP', fg='#595858', bg='#FFC3B3', width=5, font='avory 8 bold', state='disabled', command=Stop)
+    reset = tk.Button(frame, relief = FLAT, text='RESET', fg='#595858', bg='#FFC3B3', width=5, font='avory 8 bold', state='disabled', command=lambda:Reset(label))
+    label.pack()
+    start.pack()
+    stop.pack()
+    reset.pack()
+    label.place(x=30, y=480)
+    start.place(x=5, y=530)
+    stop.place(x=52, y=530)
+    reset.place(x=99, y=530)
+
+    root.mainloop()
